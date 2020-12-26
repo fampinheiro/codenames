fac0959d7c04d4307738b270cbd78dc906537c16
Filipe Pinheiro <hey@fampinheiro.com>
2020-12-26 16:20:46 +0000

    feat: remove dynamic tailwind class

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,11 +1,10 @@
 <script>
-  export let color = "green";
   export let onClick = () => {};
 </script>
 
 <button
   on:click={onClick}
-  class="place-self-center rounded shadow text-white py-2 px-4 bg-{color}-500 hover:bg-{color}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-{color}-500"
+  class="place-self-center rounded shadow text-white py-2 px-4 bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
   {...$$props}>
   <slot />
 </button>
